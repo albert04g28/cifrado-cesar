6699f52d7de8b07ef300d12367e22da1cc8204bd
albertoag2005 <apariciogallo@gmail.com>
2023-03-11 10:13:28 +0100

    hola

--- q.py
+++ q.py
@@ -1,4 +1,9 @@
-historial={}
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+
+
 abc = 'abcdefghijklmnopqrstuvwxyz_'
 
 
@@ -63,31 +68,6 @@ def main():
 
 
 
+if __name__ == '__main__':
 
-
-
-def info():
-    print("pronto info")
-
-
-ans=True
-while ans:
-    print("""
-    1 añadir un mensaje
-    2 eliminar un mensaje
-    3 info
-    4 exit/salir""")
-    ans=input("¿Que quieres hacer?")
-    if ans=="1":
-        main()
-    elif ans=="2":
-        print("en proceso")
-    elif ans=="3":
-        info()
-    elif ans=="4":
-        print("Adios")
-        break
-    elif ans!="":
-        print("no es valido intentalo de nuevo")
-
-
+    main()
